00a56c33b3d81e2ea6632f4772ad78446695c822
Anderson Benedetti <andersonjcbenedetti@gmail.com>
2025-06-26 17:21:35 -0300

    feat: add page single product

--- index.php
+++ index.php
@@ -8,16 +8,6 @@
             <div class="intro">
                 <h1 class="title"><?php the_title(); ?></h1>
             </div>
-
-            <?php
-            if (is_cart() || is_checkout()): ?>
-                <div class="delivery-info">
-                    <strong>Segunda a Sexta:</strong> pedidos feitos até 17h serão entregues no mesmo dia<br>
-                    <strong>Sábado:</strong> pedidos até 12h serão entregues no mesmo dia<br>
-                    <strong>Domingo:</strong> pedidos serão entregues na segunda
-                </div>
-            <?php endif; ?>
-
             <?php the_content(); ?>
         </main>
     <?php }
